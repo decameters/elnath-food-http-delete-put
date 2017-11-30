7b22a8a2a5c7d8afad42b73f478254d17658b3dd
liz elton <eeeqqqeee@gmail.com>
2017-11-30 09:07:56 -0600

    get working, database working

--- database.sql
+++ database.sql
@@ -3,10 +3,12 @@ CREATE DATABASE restaurant;
 CREATE TABLE food (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(20),
-	deliciousness_rating INT,
-	is_hot BOOLEAN
+	deliciousness_rating INT NOT NULL,
+	is_hot BOOLEAN NOT NULL
 );
 
--- test food
+-- test foods
 INSERT INTO food (name, deliciousness_rating, is_hot)
-VALUES ('taco', 100, true);
+VALUES ('taco', 100, true),
+('spring rolls', 100, false),
+('honey', 1, false);
